32f74e1938d7eadf92c82932a35c0ac19cb75896
Yuxiang Chen <yuxiangc@iastate.edu>
2016-12-07 12:43:55 -0600

    Add email field to User class, and modified the UserDataSource class so it won't crash.

--- app/src/main/java/com/iastate/i_attend/UsersDataSource.java
+++ app/src/main/java/com/iastate/i_attend/UsersDataSource.java
@@ -37,10 +37,11 @@ public class UsersDataSource {
         dbHelper.close();
     }
 
-    public User createUser(String userName, String userType){
+    public User createUser(String userName, String userType, String email){
         ContentValues newUserContent = new ContentValues();
         newUserContent.put(dbHelper.COLUMN_USERNAME, userName);
         newUserContent.put(dbHelper.COLUMN_USER_TYPE, userType);
+        newUserContent.put(dbHelper.COLUMN_USEREMAIL, email);
 
         long id = database.insert(dbHelper.TABLE_USERS, null, newUserContent);
 
@@ -50,7 +51,7 @@ public class UsersDataSource {
 
     public User getUser(long id){
         String[] id_string = {Long.toString(id)};
-        Cursor cursor = database.query(SQLiteHelper.TABLE_USERS, new String[] {SQLiteHelper.COLUMN_ID, SQLiteHelper.COLUMN_USERNAME, SQLiteHelper.COLUMN_USER_TYPE}, "_id=?", id_string, null, null, null);
+        Cursor cursor = database.query(SQLiteHelper.TABLE_USERS, new String[] {SQLiteHelper.COLUMN_ID, SQLiteHelper.COLUMN_USERNAME, SQLiteHelper.COLUMN_USER_TYPE, SQLiteHelper.COLUMN_USEREMAIL}, "_id=?", id_string, null, null, null);
         cursor.moveToFirst();
         User toReturn = cursorToUser(cursor);
         return toReturn;
@@ -67,7 +68,7 @@ public class UsersDataSource {
 
     private User cursorToUser(Cursor cursor){
         Log.d("Cols", "" + cursor.getColumnIndex(SQLiteHelper.COLUMN_ID) + " " + cursor.getColumnIndex(SQLiteHelper.COLUMN_USERNAME) + " " + cursor.getColumnIndex(SQLiteHelper.COLUMN_USER_TYPE));
-        User user = new User(cursor.getLong(cursor.getColumnIndex(SQLiteHelper.COLUMN_ID)), cursor.getString(cursor.getColumnIndex(SQLiteHelper.COLUMN_USERNAME)), cursor.getString(cursor.getColumnIndex(SQLiteHelper.COLUMN_USER_TYPE)));
+        User user = new User(cursor.getLong(cursor.getColumnIndex(SQLiteHelper.COLUMN_ID)), cursor.getString(cursor.getColumnIndex(SQLiteHelper.COLUMN_USERNAME)), cursor.getString(cursor.getColumnIndex(SQLiteHelper.COLUMN_USER_TYPE)), cursor.getString(cursor.getColumnIndex(SQLiteHelper.COLUMN_USEREMAIL)));
         return user;
     }
 
