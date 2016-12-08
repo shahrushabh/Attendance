eeaa763bb95a95294181ab7aea9d992b0af149ba
Yuxiang Chen <yuxiangc@iastate.edu>
2016-12-08 12:47:04 -0600

    modified:   app/src/main/java/com/iastate/i_attend/AttendanceActivity.java modified:   app/src/main/java/com/iastate/i_attend/ClassList.java modified:   app/src/main/res/layout/activity_attendance.xml new file:   app/src/main/res/layout/activity_attendance_student.xml

--- app/src/main/java/com/iastate/i_attend/AttendanceActivity.java
+++ app/src/main/java/com/iastate/i_attend/AttendanceActivity.java
@@ -8,40 +8,53 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class AttendanceActivity extends AppCompatActivity implements View.OnClickListener {
+public class AttendanceActivity extends AppCompatActivity {
 
     int courseID;
+    String type;
     TextView courseName, count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_attendance);
 
         Intent i = getIntent();
         courseID = i.getIntExtra("courseID", -1);
-        courseName = (TextView) findViewById(R.id.courseName);
-        count = (TextView) findViewById(R.id.count);
+        type = i.getStringExtra("type");
 
-        findViewById(R.id.attd).setOnClickListener(this);
+        if (type.equals(UserType.TYPE_INSTRUCTOR)) {
+            setContentView(R.layout.activity_attendance);
+            courseName = (TextView) findViewById(R.id.courseName);
+            count = (TextView) findViewById(R.id.count);
 
-    }
+            findViewById(R.id.attd).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //TODO: 1. Change Button Text
+                    Button b = (Button) view;
+                    String text = b.getText().toString();
+                    Log.d("Button Text", text);
+                    if (b.getText().toString().equals("Start")) {
+                        b.setText("Stop");
+                    } else if (b.getText().toString().equals("Stop")) {
+                        b.setText("Start");
+                    }
 
-    @Override
-    public void onClick(View view) {
+                    //TODO: 2. Start Timer
 
-        //TODO: 1. Change Button Text
-        Button b = (Button) view;
-        String text = b.getText().toString();
-        Log.d("Button Text", text);
-        if(b.getText().toString().equals("Start")){
-            b.setText("Stop");
-        } else if (b.getText().toString().equals("Stop")){
-            b.setText("Start");
+                }
+            });
+        } else if(type.equals(UserType.TYPE_STUDENT)){
+            setContentView(R.layout.activity_attendance_student);
+
+            findViewById(R.id.check).setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Button b = (Button) view;
+                    b.setText("Checked");
+                }
+            });
         }
-
-
-        //TODO: 2. Start Timer
 
     }
 }
--- app/src/main/java/com/iastate/i_attend/ClassList.java
+++ app/src/main/java/com/iastate/i_attend/ClassList.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -61,6 +60,9 @@ public class ClassList extends AppCompatActivity {
 
                 Intent intent = new Intent(ClassList.this, AttendanceActivity.class);
                 intent.putExtra("courseID", i);
+                intent.putExtra("username", username);
+                intent.putExtra("type", type);
+                intent.putExtra("email", email);
                 startActivity(intent);
 
             }
